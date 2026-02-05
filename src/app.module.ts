9c5f2f19b50636254b9666eac64d9194e8ab7dd1
Daniel Zipa <danielzipa@outlook.com>
2026-02-05 12:46:33 -0600

    Added: Structure of user, auth and admin modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [ConfigModule.forRoot(), UserModule, AdminModule, AuthModule],
 })
 export class AppModule {}
